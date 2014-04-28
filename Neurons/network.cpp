9b090d6eb3de121774f78cbeda1eb3156a1e6b87
Jeromy <jeromyj@gmail.com>
2014-04-28 20:41:07 +0000

    cleaning up some code for my sanity

--- Neurons/network.cpp
+++ Neurons/network.cpp
@@ -1,8 +1,7 @@
 #include "network.h"
 
 //Initialize network with one layer and 0 input/outputs
-Network::Network()
-{
+Network::Network() {
 	_network.resize(1);
 	numOutputs = 0;
 	numInputs = 0;
@@ -11,91 +10,71 @@ Network::Network()
 }
 
 //Set the networks learning rate
-void Network::SetLearningRate(float rate)
-{
-	for(int i = 0; i < _network.size(); i++)
-	{
-		for(int j = 0; j < _network[i].size(); j++)
-		{
+void Network::SetLearningRate(float rate) {
+	for(int i = 0; i < _network.size(); i++) {
+		for(int j = 0; j < _network[i].size(); j++) {
 			_network[i][j].setLearningRate(rate);
 		}
 	}
 }
 
 //Set the number of output nodes in the network
-void Network::SetNumOutputs(int noutp)
-{
+void Network::SetNumOutputs(int noutp) {
 	numOutputs = noutp;
 	_network[_network.size() -1].resize(noutp);
 }
 
 //Set the number of inputs to the network
-void Network::SetNumInputs(int ninp)
-{
+void Network::SetNumInputs(int ninp) {
 	numInputs = ninp;
-	for(int i = 0; i < _network[0].size(); i++)
-	{
+	for(int i = 0; i < _network[0].size(); i++) {
 		_network[0][i].setNumInputs(ninp);
 	}
 }
 
 //Set the depth of the network
-void Network::SetDepth(int depth)
-{
+void Network::SetDepth(int depth) {
 	_network.resize(depth);
 }
 
 //Set the height of the network at a given layer
-void Network::SetNodeCountAtLevel(int count, int level)
-{
+void Network::SetNodeCountAtLevel(int count, int level) {
 	_network[level].resize(count);
 }
 
 //This function sets the number of inputs in each layer to the number of neruons in the previous layer
-void Network::LinkNeurons()
-{
-	for(int i = 1; i < _network.size(); i++)
-	{
-		for(int j = 0; j < _network[i].size(); j++)
-		{
+void Network::LinkNeurons() {
+	for(int i = 1; i < _network.size(); i++) {
+		for(int j = 0; j < _network[i].size(); j++) {
 			_network[i][j].setNumInputs(_network[i-1].size());
 		}
 	}
 }
 
 //Prints each neuron in each layer and the weights of each
-void Network::PrintNetwork()
-{
-	for(int i = 0; i < _network.size(); i++)
-	{
+void Network::PrintNetwork() {
+	for(int i = 0; i < _network.size(); i++) {
 		cout << "Layer " << i << "\n";
-		for(int j = 0; j < _network[i].size(); j++)
-		{
+		for(int j = 0; j < _network[i].size(); j++) {
 			_network[i][j].print();
 		}
 	}
 }
 
 //Randomly sets the weights for each connection
-void Network::ResetNetwork()
-{
-	for(int i = 0; i < _network.size(); i++)
-	{
-		for(int j = 0; j < _network[i].size(); j++)
-		{
+void Network::ResetNetwork() {
+	for(int i = 0; i < _network.size(); i++) {
+		for(int j = 0; j < _network[i].size(); j++) {
 			_network[i][j].resetWeights(1);
 		}
 	}
 }
 
 //Applies the inputs to the network and returns the output
-vector<float> Network::Query(vector<float> inputs)
-{
-	for(int i = 0; i < _network.size(); i++)
-	{
+vector<float> Network::Query(vector<float> inputs) {
+	for(int i = 0; i < _network.size(); i++) {
 		vector<float> lInp(_network[i].size());
-		for(int j = 0; j < _network[i].size(); j++)
-		{
+		for(int j = 0; j < _network[i].size(); j++) {
 			lInp[j] = _network[i][j].snap(inputs);
 		}
 		inputs = lInp;
@@ -104,15 +83,13 @@ vector<float> Network::Query(vector<float> inputs)
 }
 
 //Train using back propogation
-void Network::Train(const vector<float> &inputs,const vector<float> &expected)
-{
+void Network::Train(const vector<float> &inputs,const vector<float> &expected) {
 	//Feed forward for results
 	vector<float> results = Query(inputs);
 
 	//Calculate output error.
 	//O * (1 - O) * (A - O) = error
-	for(int i = 0; i < results.size(); i++)
-	{
+	for(int i = 0; i < results.size(); i++) {
 		_network[_network.size() - 1][i].error = results[i] * (1 - results[i]) * (expected[i] - results[i]);
 		//Update the output weights
 		_network[_network.size() - 1][i].adjustForError();
@@ -120,14 +97,13 @@ void Network::Train(const vector<float> &inputs,const vector<float> &expected)
 	
 	//Back propogate the error
 	//decrement i to move back to the second to last layer
-	for(int i = _network.size() - 2; i >= 0; i--)
-	{
-		for(int hid = 0; hid < _network[i].size(); hid++)
-		{
+	for(int i = _network.size() - 2; i >= 0; i--) {
+		for(int hid = 0; hid < _network[i].size(); hid++) {
 			float backPropVal = 0;
 			//Summation of: (W_i * Err_i)
-			for(int bpvI = 0; bpvI < _network[i+1].size(); bpvI++)
+			for(int bpvI = 0; bpvI < _network[i+1].size(); bpvI++) {
 				backPropVal += (_network[i+1][bpvI].weights[hid] * _network[i+1][bpvI].error);
+			}
 
 			//Error = O * (1 - O) * E(W_i * Err_i)
 			_network[i][hid].error = _network[i][hid].result * (1 - _network[i][hid].result) * backPropVal;
@@ -140,16 +116,13 @@ void Network::Train(const vector<float> &inputs,const vector<float> &expected)
 
 //[Async]
 //Train using back propogation without modifying the network during training.
-vector<vector<float> > Network::TrainC(const vector<float> &inputs, const vector<float> &expected) const
-{
+vector<vector<float> > Network::TrainC(const vector<float> &inputs, const vector<float> &expected) const {
 
 	//Feed forward for results
 	vector<float> results = inputs;
-	for(int i = 0; i < _network.size(); i++)
-	{
+	for(int i = 0; i < _network.size(); i++) {
 		vector<float> lInp(_network[i].size());
-		for(int j = 0; j < _network[i].size(); j++)
-		{
+		for(int j = 0; j < _network[i].size(); j++) {
 			lInp[j] = _network[i][j].snapC(results);
 		}
 		results = lInp;
@@ -158,11 +131,11 @@ vector<vector<float> > Network::TrainC(const vector<float> &inputs, const vector
 	//Calculate output error.
 	//O * (1 - O) * (A - O) = error
 	vector<vector<float> > error(_network.size());
-	for(int i = 0; i < error.size(); i++)
+	for(int i = 0; i < error.size(); i++) {
 		error[i].resize(_network[i].size());
+	}
 	
-	for(int i = 0; i < results.size(); i++)
-	{
+	for(int i = 0; i < results.size(); i++) {
 		error[error.size() - 1][i] = results[i] * (1 - results[i]) * (expected[i] - results[i]);
 		//Update the output weights
 		//_network[_network.size() - 1][i].adjustForError(); //dont update weights for async
@@ -170,14 +143,13 @@ vector<vector<float> > Network::TrainC(const vector<float> &inputs, const vector
 	
 	//Back propogate the error
 	//decrement i to move back to the second to last layer
-	for(int i = _network.size() - 2; i >= 0; i--)
-	{
-		for(int hid = 0; hid < _network[i].size(); hid++)
-		{
+	for(int i = _network.size() - 2; i >= 0; i--) {
+		for(int hid = 0; hid < _network[i].size(); hid++) {
 			float backPropVal = 0;
 			//Summation of: (W_i * Err_i)
-			for(int bpvI = 0; bpvI < _network[i+1].size(); bpvI++)
+			for(int bpvI = 0; bpvI < _network[i+1].size(); bpvI++) {
 				backPropVal += (_network[i+1][bpvI].weights[hid] * error[i+1][bpvI]);
+			}
 
 			//Error = O * (1 - O) * E(W_i * Err_i)
 			error[i][hid] = _network[i][hid].result * (1 - _network[i][hid].result) * backPropVal;
--- Neurons/neuron.cpp
+++ Neurons/neuron.cpp
@@ -1,23 +1,19 @@
 #include "neuron.h"
 
-Neuron::Neuron()
-{
+Neuron::Neuron() {
 	learningRate = 0.5;
 }
 
-Neuron::Neuron(int numInputs, float learn)
-{
+Neuron::Neuron(int numInputs, float learn) {
 	setNumInputs(numInputs);
 	learningRate = learn;
 }
 
 //Evaluate the neuron with given inputs
-float Neuron::snap(vector<float> inputs)
-{
+float Neuron::snap(vector<float> inputs) {
 	lastInp = inputs;
 	result = 0;
-	for(int i = 0; i < inputs.size(); i++)
-	{
+	for(int i = 0; i < inputs.size(); i++) {
 		result += inputs[i] * weights[i];
 	}
 	result = sigmoid(result + zWeight);
@@ -25,60 +21,54 @@ float Neuron::snap(vector<float> inputs)
 }
 
 //Evaluate the neuron with given inputs
-float Neuron::snapC(vector<float> inputs) const
-{
+float Neuron::snapC(vector<float> inputs) const {
 	float Lresult = 0;
-	for(int i = 0; i < inputs.size(); i++)
-	{
+	for(int i = 0; i < inputs.size(); i++) {
 		Lresult += inputs[i] * weights[i];
 	}
 	Lresult = sigmoid(Lresult + zWeight);
 	return Lresult;
 }
 
-void Neuron::setLearningRate(float Nlearn)
-{
+void Neuron::setLearningRate(float Nlearn) {
 	learningRate = Nlearn;
 }
 
 //This function is here in case i want to track changes to the weight as they are applied.
-void Neuron::addToWeight(int wi, float delta)
-{
+void Neuron::addToWeight(int wi, float delta) {
 	weights[wi] += delta;
 }
 
-void Neuron::setNumInputs(int numinputs)
-{
+void Neuron::setNumInputs(int numinputs) {
 	weights.resize(numinputs);
 }
 
 //Randomizes the the weights to a number between -range and range
-void Neuron::resetWeights(int range=1)
-{
-	for(int i = 0; i < weights.size(); i++)
+void Neuron::resetWeights(int range=1) {
+	for(int i = 0; i < weights.size(); i++) {
 		weights[i] = (float)((rand() % (range * 200)) - (100 * range)) / 100.0;
+	}
 	zWeight = (float)((rand() % (range * 200)) - (100 * range)) / 100.0;
 }
 
 //Sigmoid Activation Function
-float Neuron::sigmoid(float val) const
-{
+float Neuron::sigmoid(float val) const {
 	return 1 / (1 + pow(2.71828, (val * -1)));
 }
 
-void Neuron::adjustForError()
-{
+void Neuron::adjustForError() {
 	//update the weights
 	//W = W + (n * error * IN)
-	for(int wt = 0; wt < weights.size(); wt++)
+	for(int wt = 0; wt < weights.size(); wt++) {
 		weights[wt] += learningRate * error * lastInp[wt]; 
+	}
 	zWeight += learningRate * error;
 }
 
 //Prints out all the neurons weights
-void Neuron::print()
-{
+void Neuron::print() {
 	cout << "Neuron:\n";
-	for(int i = 0; i < weights.size(); i++)
+	for(int i = 0; i < weights.size(); i++) {
 		cout << "Weight #" << i << ": " << weights[i] << "\n";
+	}
 }
